b724e26f253577a388b31a7bd5bf926697d4b20c
gwest4 <frostboss41@comcast.net>
2016-05-22 00:16:16 -0500

    removed redundant gets from TrainStopDataInstance

--- src/cta/api/TrainStopDataInstance.java
+++ src/cta/api/TrainStopDataInstance.java
@@ -2,36 +2,16 @@ package cta.api;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
-import cta.api.enums.TrainRoute;
-
 public class TrainStopDataInstance {
 	// singleton instance
 	public static TrainStopDataInstance instance = new TrainStopDataInstance();
 	
 	// fields initialized with singleton
 	public ArrayList<TrainStop> allStops;
-	public ArrayList<TrainStop> redLineStops;
-	public ArrayList<TrainStop>	blueLineStops;
-	public ArrayList<TrainStop>	greenLineStops;
-	public ArrayList<TrainStop>	brownLineStops;
-	public ArrayList<TrainStop>	purpleLineStops;
-	public ArrayList<TrainStop>	purpleExpressLineStops;
-	public ArrayList<TrainStop>	yellowLineStops;
-	public ArrayList<TrainStop>	pinkLineStops;
-	public ArrayList<TrainStop>	orangeLineStops;
 	
 	
 	private TrainStopDataInstance() {
 		this.allStops = TrainStopDataParser.parse();
-		this.redLineStops = getStopsForRoute(TrainRoute.RED_LINE);
-		this.blueLineStops = getStopsForRoute(TrainRoute.BLUE_LINE);
-		this.greenLineStops = getStopsForRoute(TrainRoute.GREEN_LINE);
-		this.brownLineStops = getStopsForRoute(TrainRoute.BROWN_LINE);
-		this.purpleLineStops = getStopsForRoute(TrainRoute.PURPLE_LINE);
-		this.purpleExpressLineStops = getStopsForRoute(TrainRoute.PURPLE_EXPRESS_LINE);
-		this.yellowLineStops = getStopsForRoute(TrainRoute.YELLOW_LINE);
-		this.pinkLineStops = getStopsForRoute(TrainRoute.PINK_LINE);
-		this.orangeLineStops = getStopsForRoute(TrainRoute.ORANGE_LINE);
 	}
 
 	public static TrainStopDataInstance getInstance() {
@@ -41,44 +21,8 @@ public class TrainStopDataInstance {
 	public ArrayList<TrainStop> getAllStops() {
 		return allStops;
 	}
-
-	public ArrayList<TrainStop> getRedLineStops() {
-		return redLineStops;
-	}
-
-	public ArrayList<TrainStop> getBlueLineStops() {
-		return blueLineStops;
-	}
-
-	public ArrayList<TrainStop> getGreenLineStops() {
-		return greenLineStops;
-	}
-
-	public ArrayList<TrainStop> getBrownLineStops() {
-		return brownLineStops;
-	}
-
-	public ArrayList<TrainStop> getPurpleLineStops() {
-		return purpleLineStops;
-	}
-
-	public ArrayList<TrainStop> getPurpleExpressLineStops() {
-		return purpleExpressLineStops;
-	}
-
-	public ArrayList<TrainStop> getYellowLineStops() {
-		return yellowLineStops;
-	}
-
-	public ArrayList<TrainStop> getPinkLineStops() {
-		return pinkLineStops;
-	}
-
-	public ArrayList<TrainStop> getOrangeLineStops() {
-		return orangeLineStops;
-	}
 	
-	public ArrayList<TrainStop> getStopsWithPredicate(Predicate<TrainStop> test) {
+	public ArrayList<TrainStop> getTrainStopsWithPredicate(Predicate<TrainStop> test) {
 		ArrayList<TrainStop> result = new ArrayList<TrainStop>();
 		for (TrainStop s : allStops) {
 			if (test.test(s)) { result.add(s); }
